4e7a439dd5bc4a0b4e31985bbc29da4c6958fa56
mateusz5564 <mateusz5564@gmail.com>
2022-10-28 13:19:37 +0200

    use tabs form WorkoutPartsNavigation

--- src/features/Workout/components/WorkoutParts/Navigation/WorkoutPartsNavigation.tsx
+++ src/features/Workout/components/WorkoutParts/Navigation/WorkoutPartsNavigation.tsx
@@ -1,24 +1,59 @@
-import { Grid } from "@mui/material";
+import { useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { Divider, SvgIcon, Tab, Tabs } from "@mui/material";
 
 import workoutPartApi from "../../../../../api/workoutPart";
-import WorkoutPartLink from "./WorkoutPartsLink";
+import { ReactComponent as PushUpsSvg } from "../../../../../assets/icons/pushups.svg";
+import { ReactComponent as LegRaisesSvg } from "../../../../../assets/icons/leg-raises.svg";
+import { ReactComponent as SquatsSvg } from "../../../../../assets/icons/squats.svg";
+import { ReactComponent as PullUpsSvg } from "../../../../../assets/icons/pullups.svg";
+import { ReactComponent as HandstandsSvg } from "../../../../../assets/icons/handstands.svg";
+import { ReactComponent as BridgesSvg } from "../../../../../assets/icons/bridges.svg";
+import { ExerciseCategory } from "../../../../../types";
 
 export default function WorkoutPartNavigation() {
+  const navigate = useNavigate();
+  const params = useParams();
   const { data: workoutParts, isError } = workoutPartApi.useFetchWorkoutParts();
+  const [value, setValue] = useState(params.category);
+
+  const icons = {
+    pushups: PushUpsSvg,
+    "leg-raises": LegRaisesSvg,
+    squats: SquatsSvg,
+    pullups: PullUpsSvg,
+    handstands: HandstandsSvg,
+    bridges: BridgesSvg,
+  };
 
   if (isError) {
     return <div>Error</div>;
   }
 
   return (
-    <Grid container spacing={2} sx={{ mb: 3 }}>
-      {workoutParts?.map(workoutPart => (
-        <Grid item xs={4} key={workoutPart.id}>
-          <WorkoutPartLink to={`${workoutPart.category}/logs`}>
-            {workoutPart.category}
-          </WorkoutPartLink>
-        </Grid>
-      ))}
-    </Grid>
+    <>
+      <Tabs
+        value={value}
+        onChange={(e, newValue) => {
+          setValue(newValue);
+        }}
+        variant="scrollable"
+        allowScrollButtonsMobile
+        aria-label="Workout parts navigation"
+      >
+        {workoutParts?.map(workoutPart => (
+          <Tab
+            key={workoutPart.category}
+            value={workoutPart.category}
+            icon={
+              <SvgIcon component={icons[workoutPart.category as ExerciseCategory]} inheritViewBox />
+            }
+            label={workoutPart.category}
+            onClick={() => navigate(`${workoutPart.category}/logs`)}
+          />
+        ))}
+      </Tabs>
+      <Divider />
+    </>
   );
 }
