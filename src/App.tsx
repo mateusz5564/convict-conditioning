dcc7f9ab926fc61a0825fae65791bebe940cdbc3
mateusz5564 <mateusz5564@gmail.com>
2023-01-18 21:04:23 +0100

    refactor:  move app routes to separate component

--- src/App.tsx
+++ src/App.tsx
@@ -1,79 +1,13 @@
-import { Navigate, Route, Routes, useLocation } from "react-router-dom";
-
 import { Container } from "@mui/material";
-import NoUser from "components/NoUser/NoUser";
-import {
-  AccountMenu,
-  LoginDialog,
-  RegisterDialog,
-  useAuthContext,
-  useBackgroundLocation,
-} from "features/Auth";
-import ChangeEmail from "features/Auth/components/ChangeEmail";
-import ChangePassword from "features/Auth/components/ChangePassword";
+import AppRoutes from "AppRoutes";
+import { AccountMenu } from "features/Auth";
 import BottomNav from "features/BottomNav";
-import {
-  ExerciseInstructions,
-  ExerciseLogs,
-  WorkoutPart,
-} from "features/Workout";
-import NewPassword from "pages/NewPassword";
-
-import AccountSettings from "./pages/AccountSettings";
-import Overview from "./pages/Overview";
-import WorkoutParts from "./pages/WorkoutParts";
 
 const App = () => {
-  const user = useAuthContext();
-  const location = useLocation();
-  const backgroundLocation = useBackgroundLocation();
-
-  const appLocation =
-    backgroundLocation ||
-    (location.pathname === "/register" || location.pathname === "/login"
-      ? "/"
-      : location);
-
   return (
     <Container sx={{ padding: "12px", paddingBottom: "80px" }}>
       <AccountMenu />
-      <Routes location={appLocation}>
-        <Route path="/" element={<Overview />} />
-        <Route path="/workout-parts" element={<WorkoutParts />}>
-          <Route path=":category" element={<WorkoutPart />}>
-            <Route index element={<Navigate to="logs" />} />
-            <Route
-              path="logs"
-              element={
-                user ? (
-                  <ExerciseLogs />
-                ) : (
-                  <NoUser text="You must be logged in to view and add logs" />
-                )
-              }
-            />
-            <Route path="instructions" element={<ExerciseInstructions />} />
-          </Route>
-        </Route>
-        <Route
-          path="account"
-          element={user ? <AccountSettings /> : <Navigate to="/" />}
-        >
-          <Route index element={<Navigate to="change-password" />} />
-          <Route path="change-password" element={<ChangePassword />} />
-          <Route path="change-email" element={<ChangeEmail />} />
-        </Route>
-        <Route path="/recover" element={<NewPassword />} />
-        <Route path="/login" element={<Overview />} />
-        <Route path="/register" element={<Overview />} />
-        <Route path="*" element={<div>Page not found</div>} />
-      </Routes>
-
-      <Routes>
-        <Route path="/login" element={<LoginDialog />} />
-        <Route path="/register" element={<RegisterDialog />} />
-        <Route path="*" element={null} />
-      </Routes>
+      <AppRoutes />
       <BottomNav />
     </Container>
   );
