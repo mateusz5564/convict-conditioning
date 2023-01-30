18d35111f49cdf2969ebd9084e6dde27e2a625fd
mateusz5564 <mateusz5564@gmail.com>
2023-01-30 21:48:24 +0100

    refactor: remove unnecessary folder

--- src/features/Workout/index.tsx
+++ src/features/Workout/index.tsx
@@ -1,7 +1,7 @@
 import ExerciseInstructions from "./components/Exercises/Instructions/ExerciseInstructions";
 import ExerciseLogs from "./components/Exercises/Logs/ExerciseLogs";
-import WorkoutPartsNavigation from "./components/WorkoutParts/Navigation/WorkoutPartsNavigation";
 import WorkoutPart from "./components/WorkoutParts/WorkoutPart";
+import WorkoutPartsNavigation from "./components/WorkoutParts/WorkoutPartsNavigation";
 
 export {
   ExerciseLogs,
